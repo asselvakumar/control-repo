8d73b0d3482b12ce250bc8b32ca015d08c0bbb46
Selvakumar <37397672+asselvakumar@users.noreply.github.com>
2019-09-09 17:01:00 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,3 +29,26 @@ node default {
   # Example:
   #   class { 'my_class': }
 }
+
+node 'pe-201819-agent-lb.puppetdebug.vlan' {
+  include ::haproxy
+  haproxy::listen { 'puppet00':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '8140',
+  }
+  haproxy::balancermember { 'master00':
+    listening_service => 'puppet00',
+    server_names      => 'master00.example.com',
+    ipaddresses       => '10.0.0.10',
+    ports             => '8140',
+    options           => 'check',
+  }
+  haproxy::balancermember { 'master01':
+    listening_service => 'puppet00',
+    server_names      => 'master01.example.com',
+    ipaddresses       => '10.0.0.11',
+    ports             => '8140',
+    options           => 'check',
+  }
+}
